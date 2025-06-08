6b5482d68c82387b6a77113378678e594dc7d633
Leander Furumo <leander@furumo.eu>
2025-06-08 02:11:01 +0200

    add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,18 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://furumo.eu",
   integrations: [mdx(), sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
